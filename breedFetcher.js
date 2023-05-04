69463a877330866fdecb688d08ff0fa3a3f1778a
sselinatoledo <sselinatoledo@gmail.com>
2023-05-04 11:39:06 +0000

    update breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,24 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(`Failed to request: ${error}`);
-    return;
-  }
-    
-  const data = JSON.parse(body);
-  const breed = data[0];
-    
-  if (!breed) {
-    console.log(`Breed '${breedName}' not found`);
-    return;
-  }
-    
-  console.log(breed.description);
-});
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null)
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback(`Breed ${breedName} not found.`, null);
+      return;
+    }
+
+    const description = data[0].description;
+    callback(null, description);
+  });
+};
+
+module.exports = { fetchBreedDescription };

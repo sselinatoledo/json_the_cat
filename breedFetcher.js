c02af6983b2053fd8bf50e38ee983989f7677282
sselinatoledo <sselinatoledo@gmail.com>
2023-05-04 10:52:14 +0000

    eslint breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,18 +4,18 @@ const breedName = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 request(url, (error, response, body) => {
-    if (error) {
-      console.log(`Failed to request: ${error}`);
-      return;
-    }
+  if (error) {
+    console.log(`Failed to request: ${error}`);
+    return;
+  }
     
-    const data = JSON.parse(body);
-    const breed = data[0];
+  const data = JSON.parse(body);
+  const breed = data[0];
     
-    if (!breed) {
-      console.log(`Breed '${breedName}' not found`);
-      return;
-    }
+  if (!breed) {
+    console.log(`Breed '${breedName}' not found`);
+    return;
+  }
     
-    console.log(breed.description);
-  });
+  console.log(breed.description);
+});
